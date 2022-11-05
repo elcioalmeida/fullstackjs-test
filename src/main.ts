c5fd00fc6a4317a60e41fb243f0a33dbf5a70dc9
Elcio Almeida <elcio.almeida78@gmail.com>
2022-11-05 15:37:08 -0300

    new api tests added

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,14 @@ app.get("/boards", async function (req, res) {
     console.log(boards);
     res.json(boards);
 });
-app.get("/boards", async function (req, res) {
-    const boards = await connection.query("select * from board", []);
-    console.log(boards);
-    res.json(boards);
+app.get("/boards/:idBoard/columns", async function (req, res) {
+    const columns = await connection.query("select * from column_a where id_board = $1", [req.params.idBoard]);
+    console.log(columns);
+    res.json(columns);
+});
+app.get("/boards/:idBoard/columns/:idColumn/cards", async function (req, res) {
+    const cards = await connection.query("select * from card where id_column = $1", [req.params.idColumn]);
+    console.log(cards);
+    res.json(cards);
 });
 app.listen(3000);
